1f00fdf2d847c07352c693bca7a251dd46001a62
Tegibar1983 <tbtf1991@gmail.com>
2024-05-02 11:24:51 +0000

    Python Advanced

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-"""  A type-annotated function named 'floor that takes argument 'n' and returned a float fllor """
+''' A type-annotated function named floor that takes argument n and returned a float fllor '''
 
 def floor(a: float) -> int:
 
-    """ compute floor of floting point number
-    """
+    ''' compute floor of floting point number
+    '''
     return int(a)
