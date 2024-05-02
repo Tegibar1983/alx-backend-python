5b5277ef22305d33261a32096f8228a4de4f66ab
Tegibar1983 <tbtf1991@gmail.com>
2024-05-02 11:04:56 +0000

    Python advanced

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+'''Task 0's module.
+'''
 
-""" A function that add 2 float numbers and returns a float """
 
 def add(a: float, b: float) -> float:
     '''Adds two floating-point numbers.
