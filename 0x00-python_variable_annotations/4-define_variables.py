efb566dcdd71cd3f66154ad8cc034001d480b5e1
Tegibar1983 <tbtf1991@gmail.com>
2024-05-02 06:57:24 +0000

    4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/user/bin/env python3
+
+""" A Define and annotate a variable with specific value """
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
