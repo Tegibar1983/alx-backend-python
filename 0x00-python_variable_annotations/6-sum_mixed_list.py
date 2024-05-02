2c69c31db178e6c78e793c2dcd24c88c4b57270c
Tegibar1983 <tbtf1991@gmail.com>
2024-05-02 10:56:16 +0000

    Python Advanced

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-
-""" A function that computes the sum of 2 mixed type nums and return sum as float
-"""
-
+'''Task 6's module.
+'''
 from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''Computes the sum of a list of integers and floating-point numbers.
+    '''
     return float(sum(mxd_lst))
