8a3b49e4a878945cd919a31a81bc9b727ed27a39
Tegibar1983 <tbtf1991@gmail.com>
2024-05-02 11:17:38 +0000

    Python Advanced

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-""" Task 1's module 
-"""
+'''Task 1's module.
+'''
+
 
 def concat(str1: str, str2: str) -> str:
-
-    """concatenates 2 strings 
-    """
-
+    '''Concatenates two strings.
+    '''
     return str1 + str2
