28255aebb9eb1049908f05be07f2f259b4d04d5b
Tegibar1983 <tbtf1991@gmail.com>
2024-05-07 11:27:26 +0000

    Async Comprehensions

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+'''
+Python Async Compreheshions
+'''
+
+from asyncio import sleep
+from random import uniform
+from typing import Generator
+
+async def async_generator() -> Generator[float, None, None]:
+    '''
+    Async Generator
+    '''
+
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
